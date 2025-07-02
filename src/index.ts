bee6a650bbaa70ca87f2b55af5ebae4262ce3677
windbullet <2536810643@qq.com>
2025-07-02 10:30:27 +0800

    首次提交

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Context, Schema } from 'koishi'
+import {} from "koishi-plugin-adapter-onebot"
 
 export const name = 'set-essence-message'
 
@@ -7,5 +8,9 @@ export interface Config {}
 export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command("加精", "将被引用的消息设为精华", {captureQuote: false})
+    .action(async ({session}) => {
+      if (!session.quote) return "需要引用一条消息"
+      await session.onebot.setEssenceMsg(session.quote.id)
+    })
 }
