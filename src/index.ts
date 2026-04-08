210d8a610dcfde5ff6aeea5cfe865b19dfd960d6
VincentZyuwin11台式机 <VincentZyuwin11>
2026-04-08 23:39:01 +0800

    feat: add 去精功能 & chore: bump version to 1.0.3

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,10 @@ export function apply(ctx: Context) {
       if (!session.quote) return `${h.quote(session?.messageId)}需要引用一条消息`
       await session.onebot.setEssenceMsg(session.quote.id)
     })
+
+  ctx.command('去精', '取消被引用消息的精华状态', { captureQuote: false })
+    .action(async ({ session }) => {
+      if (!session.quote) return `${h.quote(session?.messageId)}需要引用一条消息`
+      await session.onebot.deleteEssenceMsg(session.quote.id)
+    })
 }
