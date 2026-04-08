d42b2d70f3a8bd03b400904fcf38b9cd302b3d7f
VincentZyuwin11台式机 <VincentZyuwin11>
2026-04-08 23:27:13 +0800

    feat: add quote 消息段 对于 没有quote的指令， chore: bump version to 1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema } from 'koishi'
+import { Context, h, Schema } from 'koishi'
 import {} from "koishi-plugin-adapter-onebot"
 
 export const name = 'set-essence-message'
@@ -10,7 +10,7 @@ export const Config: Schema<Config> = Schema.object({})
 export function apply(ctx: Context) {
   ctx.command("加精", "将被引用的消息设为精华", {captureQuote: false})
     .action(async ({session}) => {
-      if (!session.quote) return "需要引用一条消息"
+      if (!session.quote) return `${h.quote(session?.messageId)}需要引用一条消息`
       await session.onebot.setEssenceMsg(session.quote.id)
     })
 }
