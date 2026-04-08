e05234d874d20003808d2d1b8385bebfacb4658b
84 bawuyinguo root <1830540513zyu@gmail.com>
2026-04-08 23:44:56 +0800

    chore: bump version to 1.0.4
    - Add documentation comments for OneBot adapter essence message methods
    
    - Update package version from 1.0.3 to 1.0.4

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,22 @@ export function apply(ctx: Context) {
       await session.onebot.deleteEssenceMsg(session.quote.id)
     })
 }
+
+
+/**
+ * 
+ * onebot适配器的封装：
+root@bawuyinguo:/home/bawuyinguo/SSoftwareFiles/koishi/AAA_from_git_AAA/koishi-plugin-adapter-onebot# rg -i EssenceMsg
+readme.md
+121:- [`onebot.setEssenceMsg()`](https://docs.go-cqhttp.org/api/#设置精华消息)
+122:- [`onebot.deleteEssenceMsg()`](https://docs.go-cqhttp.org/api/#移出精华消息)
+123:- [`onebot.getEssenceMsgList()`](https://docs.go-cqhttp.org/api/#获取精华消息列表)
+
+src/types.ts
+421:  setEssenceMsg(message_id: id): Promise<void>
+422:  setEssenceMsgAsync(message_id: id): Promise<void>
+423:  deleteEssenceMsg(message_id: id): Promise<void>
+424:  deleteEssenceMsgAsync(message_id: id): Promise<void>
+432:  getEssenceMsgList(group_id: id): Promise<EssenceMessage[]>
+root@bawuyinguo:/home/bawuyinguo/SSoftwareFiles/koishi/AAA_from_git_AAA/koishi-plugin-adapter-onebot# 
+ */
